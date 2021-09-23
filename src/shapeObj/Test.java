64700a484a641522dac52e480d6123f86a4cb57a
cuongtien00 <cuonghiep000@gmail.com>
2021-09-23 11:24:11 +0700

    add shapeObj

---
+++ src/shapeObj/Test.java
@@ -0,0 +1,9 @@
+package shapeObj;
+
+public class Test {
+    public static void main(String[] args) {
+       Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle.getArea());
+
+    }
+}
